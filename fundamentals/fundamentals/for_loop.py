5b850520887a4d1a2101e2939f492c8af6b4a272
rahal <rahalnoussayerr@gmail.com>
2023-10-29 14:45:10 +0100

    add

---
+++ fundamentals/fundamentals/for_loop.py
@@ -0,0 +1,25 @@
+for i in range (1,151):
+    print(i)
+for i in range (1,1000):
+    if (i%5)==0:
+        print(i)
+for i in range (1,101):
+    if(i%10)==0:
+        print("coding dojo")
+    elif (i%5)==0:
+        print("coding")
+    else:
+        print (i)
+somme=0
+for i in range(1,500):
+    if (i%2)==0:
+        somme=somme+i
+print(somme)
+for i in range (2018,0,-4):
+    print(i)
+lownum=2
+highnum=9
+mult=3
+for i in range(lownum,highnum+1,1):
+    if (i%mult)==0:
+        print(i)
