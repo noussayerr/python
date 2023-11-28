b1f379ebcf3d844bf91ac80b70330166f6074bc5
rahal <rahalnoussayerr@gmail.com>
2023-11-28 14:56:38 +0100

    add

---
+++ flask_mysql/crud/core/dojos_ninjas/flask_app/controllers/dojos.py
@@ -0,0 +1,17 @@
+from flask_app.models.dojo import dojo
+from flask import render_template , request, redirect
+from flask_app import app
+
+@app.route('/dojos')
+def index():
+    dojos_list=dojo.show()
+    return render_template("index.html",dojos=dojos_list)
+@app.route('/new_dojo',methods=['POST'])
+def new_dojo():
+    data=request.form
+    dojo.new_dojo(data)
+    return redirect('/dojos')
+@app.route('/new_ninja')
+def add_ninja_page():
+    dojos_list=dojo.show()
+    return render_template("new_ninja.html",dojos=dojos_list)
--- flask_mysql/crud/core/dojos_ninjas/flask_app/controllers/ninjas.py
+++ flask_mysql/crud/core/dojos_ninjas/flask_app/controllers/ninjas.py
@@ -1,21 +1,7 @@
-from flask_app.models.dojo import dojo
+
 from flask_app.models.ninja import ninja
 from flask import render_template , request, redirect
 from flask_app import app
-
-@app.route('/dojos')
-def index():
-    dojos_list=dojo.show()
-    return render_template("index.html",dojos=dojos_list)
-@app.route('/new_dojo',methods=['POST'])
-def new_dojo():
-    data=request.form
-    dojo.new_dojo(data)
-    return redirect('/dojos')
-@app.route('/new_ninja')
-def add_ninja_page():
-    dojos_list=dojo.show()
-    return render_template("new_ninja.html",dojos=dojos_list)
 
 @app.route('/new/ninja',methods=['POST'])
 def new_ninja():
