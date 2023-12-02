de9d1f9e6b6cec4878bb9952f0666edbbafbcc79
rahal <rahalnoussayerr@gmail.com>
2023-12-02 15:34:10 +0100

    add

---
+++ flask_mysql/validation/practice/Email Validation with DB/flask_app/controllers/emails.py
@@ -0,0 +1,19 @@
+from flask_app.models.email import email
+from flask import render_template , request, redirect,session
+from flask_app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/add',methods=['POST'])
+def add():
+    data=request.form
+    if not email.validate_email(data):
+        return redirect('/')
+    email.create(data)
+    return redirect('/show')
+
+@app.route('/show')
+def show():
+    emails=email.show()
+    return render_template("show.html",emails=emails)
---
+++ flask_mysql/validation/practice/Email Validation with DB/flask_app/models/email.py
@@ -0,0 +1,48 @@
+from flask_app.config.mysqlconnection import connectToMySQL, DB
+from flask import flash
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+class email:
+    def __init__(self,data) :
+        self.id=data['id']
+        self.email=data['email']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+    
+    @classmethod
+    def create(cls,data):
+        query="insert into emails (email) values (%(email)s);"
+        results=connectToMySQL(DB).query_db(query, data)
+        return results
+    @classmethod
+    def show(cls):
+        query="SELECT * FROM emails;"
+        results=connectToMySQL(DB).query_db(query)
+        emails=[]
+        for row in results:
+            email=cls(row)
+            emails.append(email)
+        return emails 
+    @classmethod
+    def get_by_email(cls , data):
+        query = "SELECT * FROM emails WHERE email = %(email)s;"
+        result = connectToMySQL(DB).query_db(query , data)
+        if result:
+            return cls(result[0])
+        return False
+    @classmethod
+    def delete(cls,data):
+        query="delete from emails where email=(%(email)s);"
+        results=connectToMySQL(DB).query_db(query, data)
+        return results
+    @staticmethod
+    def validate_email(data):
+        is_valid = True 
+        email_in_db = email.get_by_email(data)
+        if not EMAIL_REGEX.match(data['email']): 
+            flash("Invalid email address!")
+            is_valid = False
+        if email_in_db:
+            flash("A user with the provided email already exists.")
+            is_valid = False
+        return is_valid
